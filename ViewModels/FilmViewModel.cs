ae000a375e7ebdde59232a5d2544b32209a4d171
Malin Johansson <johanssonmalin45@gmail.com>
2025-09-10 08:43:36 +0200

    Gjort FilmViewModel

---
+++ ViewModels/FilmViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Retro_grupp_g.ViewModels
+{
+    public class FilmViewModel
+    {
+        public int FilmId { get; set; }
+
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public List<string>Genres { get; set; } = new(); //För att visa om en film ligger i flera genrer
+        public int? ReleaseYear { get; set; } 
+        public int? Length { get; set; } 
+        public string Rating { get; set; } = "";
+        public string Language { get; set; } = "";
+        public string ActorSummary { get; set; } = "";
+
+
+    }
+}
