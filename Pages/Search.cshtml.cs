94e3c27f69620dbe4e93d4a725bb33e6cb25d2ec
Malin Johansson <johanssonmalin45@gmail.com>
2025-09-14 11:06:13 +0200

    FIxat startsida, lagt css direkt på sidan istället för att ändra i css:en (kanske denna css ej behövs, kanske något går att använda?), skapat tomma sidor med länkar till de sidor som hänvisas till på startsidan (dvs ej gjort något innehåll till de sidorna)

---
+++ Pages/Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Retro_grupp_g.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
