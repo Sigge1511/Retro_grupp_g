7767133781ff714176cfa8f8eeb1a22607d9725f
Nellie Juhlin <nellie1997@hotmail.com>
2025-09-16 10:07:29 +0200

    Fixed repository + edit

--- Pages/Customers/Edit.cshtml.cs
+++ Pages/Customers/Edit.cshtml.cs
@@ -1,12 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Retro_grupp_g.Data;
+using Retro_grupp_g.Repositories;
+using Retro_grupp_g.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Retro_grupp_g.Pages.Customers
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ICustomerRepository _repo;
+        private readonly SakilaDbContext _db;   // dropdowns
+
+        public EditModel(ICustomerRepository repo, SakilaDbContext db)
         {
+            _repo = repo;
+            _db = db;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = null!;
+
+        public SelectList Stores { get; set; } = null!;
+        public SelectList Addresses { get; set; } = null!;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var cust = await _repo.GetByIdAsync(id);
+            if (cust == null) return NotFound();
+
+            Customer = cust;
+            LoadDropdowns();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadDropdowns(); 
+                return Page();
+            }
+
+            await _repo.UpdateAsync(Customer);
+            await _repo.SaveAsync();
+
+            return RedirectToPage("Index");
+        }
+
+        private void LoadDropdowns()
+        {
+            var stores = _db.Stores
+                .OrderBy(s => s.StoreId)
+                .Select(s => new { s.StoreId, Text = $"Store {s.StoreId}" })
+                .ToList();
+            Stores = new SelectList(stores, "StoreId", "Text", Customer?.StoreId);
+
+            var addresses = _db.Addresses
+                .OrderBy(a => a.AddressId)
+                .Select(a => new { a.AddressId, Text = $"{a.AddressId} - {a.Address1}" })
+                .ToList();
+            Addresses = new SelectList(addresses, "AddressId", "Text", Customer?.AddressId);
         }
     }
 }
--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -4,38 +4,46 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Retro_grupp_g.Data;
 using Retro_grupp_g.Models;
+
 namespace Retro_grupp_g.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
-
         private readonly SakilaDbContext _db;
         public CustomerRepository(SakilaDbContext db) => _db = db;
-     
+
         public Task AddAsync(Customer customer) =>
             _db.Customers.AddAsync(customer).AsTask();
 
         public async Task DeleteAsync(int id)
         {
-            var c = await GetByIdAsync(id);
+            var c = await _db.Customers.FindAsync((ushort)id);
             if (c != null) _db.Customers.Remove(c);
         }
 
         public Task<List<Customer>> GetAllAsync() =>
-            _db.Customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToListAsync();
-
+            _db.Customers
+               .OrderBy(c => c.LastName)
+               .ThenBy(c => c.FirstName)
+               .ToListAsync();
 
         public Task<Customer?> GetByIdAsync(int id) =>
-            _db.Customers.FindAsync(id).AsTask();
-
+            _db.Customers.FindAsync((ushort)id).AsTask();
 
         public Task SaveAsync() => _db.SaveChangesAsync();
-
 
         public Task UpdateAsync(Customer customer)
         {
             _db.Customers.Update(customer);
             return Task.CompletedTask;
+        }
+        public async Task<Customer?> GetDetailsAsync(ushort id)
+        {
+            return await _db.Customers
+                .Include(c => c.Rentals)
+                    .ThenInclude(r => r.Inventory)
+                        .ThenInclude(i => i.Film)
+                .FirstOrDefaultAsync(c => c.CustomerId == id);
         }
     }
 }
