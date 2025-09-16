0705b0f0d3593ac778abb803a4a07dfce7e46f6b
Nellie Juhlin <nellie1997@hotmail.com>
2025-09-16 09:15:21 +0200

    CRUD (index + create)

---
+++ Pages/Customers/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Retro_grupp_g.Pages.Customers
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
