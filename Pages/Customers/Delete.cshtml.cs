3e638a6a95a74afc5aeef32d17b484fbefc2ec44
Nellie Juhlin <nellie1997@hotmail.com>
2025-09-16 16:41:49 +0200

    Details + Delete + small fixes

--- Pages/Customers/Delete.cshtml.cs
+++ Pages/Customers/Delete.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Retro_grupp_g.Repositories;
+using Retro_grupp_g.Models;
 
 namespace Retro_grupp_g.Pages.Customers
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ICustomerRepository _repo;
+        public DeleteModel(ICustomerRepository repo) => _repo = repo;
+
+        [BindProperty]
+        public Customer Customer { get; set; } = new();
+        public async Task<IActionResult> OnGetAsync(int id)
         {
+            var c = await _repo.GetByIdAsync(id);
+            if (c == null) return NotFound();
+            Customer = c;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            await _repo.DeleteAsync(id);
+            await _repo.SaveAsync();
+            return RedirectToPage("Index");
         }
     }
 }
--- Pages/Customers/Details.cshtml.cs
+++ Pages/Customers/Details.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Retro_grupp_g.Repositories;
+using Retro_grupp_g.Models;
 
 namespace Retro_grupp_g.Pages.Customers
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ICustomerRepository _repo;
+        public DetailsModel(ICustomerRepository repo) => _repo = repo;
+        public Customer Customer {  get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(int id)
         {
+            var customer = await _repo.GetDetailsAsync((ushort)id);
+            if (customer == null) return NotFound();
+
+            Customer = customer;
+            return Page();
         }
     }
 }
