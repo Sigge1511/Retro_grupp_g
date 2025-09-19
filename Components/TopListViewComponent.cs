eb1f168f61693ce00631ea0c2b5665f18e62190c
Smockelito <amandavikberg90@gmail.com>
2025-09-19 11:06:12 +0200

    Folder and started ViewComponent

---
+++ Components/TopListViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Retro_grupp_g.Repositories;
+
+namespace Retro_grupp_g.Components
+{
+    public class TopListViewComponent : ViewComponent
+    {
+        private readonly IFilmRepository _filmRepository;
+
+        public TopListViewComponent(IFilmRepository filmRepository)
+        {
+            this._filmRepository = filmRepository;
+        }
+    }
+}
