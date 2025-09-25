25a35016b6921ed89e1c32fbc78e018ed7efcee5
Maja Sigfeldt <msigfeldt@gmail.com>
2025-09-26 00:16:14 +0200

    Made the mock page

--- Pages/Help.cshtml.cs
+++ Pages/Help.cshtml.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Retro_grupp_g.Data;
+using Retro_grupp_g.Models;
 
 namespace Retro_grupp_g.Pages
 {
